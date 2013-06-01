8ace10aa573af6eb79484383c5e6606cc10aab44
Jeevan Gulab Pawar <jeevan.wavetechline@gmail.com>
2013-06-01 15:56:21 +0530

    loginshadow

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
 <H2 align="left">CHATURANG TOURS PVT LTD</H2>
 <br />
 <br />
-<div id="login-box-name" style="margin-top:20px;">Email:</div><div id="login-box-field" style="margin-top:20px;"><input name="q" class="form-login" title="Username" value="" size="30" maxlength="2048" /></div>
-<div id="login-box-name">Password:</div><div id="login-box-field"><input name="q" type="password" class="form-login" title="Password" value="" size="30" maxlength="2048" /></div>
+<div id="login-box-name" style="margin-top:20px;">User:</div><div id="login-box-field" style="margin-top:20px;"><input name="q" class="form-login" title="Username" value="Enter User Name" size="30" maxlength="2048" /></div>
+<div id="login-box-name">Password:</div><div id="login-box-field"><input name="q" type="password" class="form-login" title="Password" value="Enter Password" size="30" maxlength="2048" /></div>
 <br />
 <span class="login-box-options"><input type="checkbox" name="1" value="1"> Remember Me <a href="#" style="margin-left:30px;">Forgot password?</a></span>
 <br />
