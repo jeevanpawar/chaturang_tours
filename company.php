ab427b56962d625aeafacd73a04709b0e1324f0c
Jeevan Gulab Pawar <jeevan.wavetechline@gmail.com>
2013-06-01 15:42:03 +0530

    login

---
+++ company.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Chaturang</title>
+
+<link href="login-box.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+
+<div style="padding: 100px 0 0 0;" align="center">
+<form action="home.php" method="post">
+
+<div id="login-box">
+
+<H2 align="left">CHATURANG TOURS PVT LTD</H2>
+<br />
+<br />
+<div id="login-box-field" style="margin-top:20px;"><label id="select">Select Company</label></div>
+<div id="login-box-field-select">
+<select>
+<option>Chaturang Tours</option>
+<option>Chaturang Tours Pvt Ltd</option>
+<option>Chaturang Holidays</option>
+</select>
+</div>
+<br />
+<span class="login-box-options"></span>
+<br />
+<br />
+<div  ><input class="logcom" type="submit" value="Go" /></div>
+
+
+
+</form>
+
+
+</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
